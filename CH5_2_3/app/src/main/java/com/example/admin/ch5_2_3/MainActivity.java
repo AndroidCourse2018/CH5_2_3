9cc44f3bee4ee8c8c9b45dbc4ed907f9be7c8931
MLR-SUN <sun.ios06444@gmail.com>
2018-03-20 11:37:33 +0800

    onCreate()

--- CH5_2_3/app/src/main/java/com/example/admin/ch5_2_3/MainActivity.java
+++ CH5_2_3/app/src/main/java/com/example/admin/ch5_2_3/MainActivity.java
@@ -8,10 +8,11 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "CH5_2_3"; // 宣告常數 TAG
 
-    @Override
+    @Override //第一個覆寫方法 onCreate
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, " ActivityCh5_2_3 : onCreate "); // 使用 Log.d()方法輸出呼叫此方法的訊息文字
     }
 
 }
