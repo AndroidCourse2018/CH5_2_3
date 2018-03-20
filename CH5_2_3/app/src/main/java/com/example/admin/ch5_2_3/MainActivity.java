2835dbd04affcc367a2d1ea69d8a3fbfb948195c
MLR-SUN <sun.ios06444@gmail.com>
2018-03-20 11:34:59 +0800

    使用Log類別方法、宣告常數

--- CH5_2_3/app/src/main/java/com/example/admin/ch5_2_3/MainActivity.java
+++ CH5_2_3/app/src/main/java/com/example/admin/ch5_2_3/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.admin.ch5_2_3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;  // 使用Log類別方法顯示訊息
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "CH5_2_3"; // 宣告常數 TAG
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
